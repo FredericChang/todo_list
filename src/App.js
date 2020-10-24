453f809432366eee95ea0635a89646f782302320
Frederic <paulpoiu123@gmail.com>
2020-10-24 21:28:09 +0800

    Applying Child Components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { TodoBanner } from "./components/TodoBanner"; 
+import { TodoCreator } from "./components/TodoCreator"; 
+import { TodoRow } from "./components/TodoRow"; 
 // import logo from './logo.svg';
 // import './App.css';
 export default class App extends Component { 
@@ -10,7 +13,6 @@ export default class App extends Component {
                     { action: "Get Shoes", done: false },
                     { action: "Collect Tickets", done: true },
                     { action: "Call Joe", done: false }],
-        newItemText: ""
     }
   }
 
@@ -19,58 +21,53 @@ export default class App extends Component {
     this.setState({ newItemText: event.target.value});
 
   }
-  createNewTodo = () => {
-    if (!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
-                this.setState({
-                  todoItems: [ ...this.state.todoItems,
-                    {action: this.state.newItemText, done: false}],
-                  newItemText: "",
-                });
-              }
-  }
-
+  // createNewTodo = () => {
+  //   if (!this.state.todoItems.find(item => item.action === this.state.newItemText)) {
+  //               this.setState({
+  //                 todoItems: [ ...this.state.todoItems,
+  //                   {action: this.state.newItemText, done: false}],
+  //                 newItemText: "",
+  //               });
+  //             }
+  // }
+  createNewTodo = (task) => {
+    if (!this.state.todoItems.find(item => item.action === task)) {
+        this.setState({
+            todoItems: [...this.state.todoItems, { action: task, done: false }]
+        });
+    }
+}
  
   
 
   toggleTodo = (todo) => this.setState({ todoItems:
       this.state.todoItems.map(item => item.action === todo.action
           ? { ...item, done: !item.done} : item) });
-  todoTableRows = () => this.state.todoItems.map(item => 
-    <tr key={ item.action }>
-      <td> { item.action } </td>
-      <td>
-        <input type = "checkbox" checked= { item.done }
-          onChange={() => this.toggleTodo(item)} />
-      </td>
-    </tr>);
+  
+  todoTableRows = () => this.state.todoItems.map(item =>
+       <TodoRow key={ item.action } 
+           item={ item } 
+           callback={ this.toggleTodo } 
+       />
+  )
   // changeStateData = () => {
   //   this.setState ({
   //     userName: this.state.userName === "Adam" ? "Bob" : "Adam"
   //   })
   // }
     render = () =>
-            <div>
-                <h4 className="bg-primary text-white text-center p-2">
-                    { this.state.userName }'s To Do List
-                    ({ this.state.todoItems.filter(t => !t.done).length} items to do)
-                </h4>
-                <div className="container-fluid">
-                    <div className="my-1">
-                      <input className="form-control"
-                          value={ this.state.newItemText }
-                          onChange={ this.updateNewTextValue } />
-
-                      <button className="btn btn-primary mt-1"
-                          onClick={ this.createNewTodo }>Add</button>
-                    </div>
-                    <table className="table table-striped table-bordered">
-                      <thead>
-                        <tr><th>Description</th><th>Done</th></tr>
-                      </thead>
-                      <tbody>{ this.todoTableRows() }</tbody>
-                    </table>
-                </div>
-            </div>
+    <div>
+    <TodoBanner name={ this.state.userName } tasks={this.state.todoItems } />
+    <div className="container-fluid">
+        <TodoCreator callback={ this.createNewTodo } />
+        <table className="table table-striped table-bordered">
+            <thead>
+                <tr><th>Description</th><th>Done</th></tr>
+            </thead>
+            <tbody>{ this.todoTableRows() }</tbody>
+        </table>
+    </div>
+    </div>
 }
 
 
